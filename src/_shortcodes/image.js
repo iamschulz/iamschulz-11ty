97127acb8544ebacf94f84e97ace893ff774fb00
Daniel Schulz <hallo@iamschulz.de>
2022-10-17 23:16:42 +0200

    get images right

--- src/_shortcodes/image.js
+++ src/_shortcodes/image.js
@@ -1,11 +1,16 @@
-const Image = require('@11ty/eleventy-img');
+const Image = require("@11ty/eleventy-img");
 
-module.exports = function imageShortcode(src, alt) {
+module.exports = function imageShortcode(
+	src,
+	alt,
+	className = "",
+	lazy = true
+) {
 	// todo: add whitelist
 	let options = {
-		widths: [300, 600],
-		formats: ['avif', 'webp', 'jpeg'],
-		outputDir: './dist/img/',
+		widths: [420, 786, 1000],
+		formats: ["avif", "webp", "jpeg"],
+		outputDir: "./dist/img/",
 	};
 
 	// generate images, while this is async we don’t wait
@@ -13,14 +18,17 @@ module.exports = function imageShortcode(src, alt) {
 
 	let imageAttributes = {
 		alt,
-		sizes: '(min-width: 30em) 50vw, 100vw',
-		loading: 'lazy',
-		decoding: 'async',
+		sizes: "100vw",
+		class: className,
+		loading: lazy ? "lazy" : "eager",
+		decoding: "async",
 	};
 
+	console.log(src);
+
 	// get metadata even the images are not fully generated
-	const metadata = Image.statsByDimensionsSync(src, 600, 600, options);
+	const metadata = Image.statsByDimensionsSync(src, 1000, 1000, options);
 	let html = Image.generateHTML(metadata, imageAttributes);
-	html = html.replace(/(?:width|height)="[0-9]+"/gm, '');
+	html = html.replace(/(?:width|height)="[0-9]+"/gm, "");
 	return html;
 };
