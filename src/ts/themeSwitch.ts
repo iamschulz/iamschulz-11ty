6e40690107dbd0511e2114843f879def2ebecf77
Daniel Schulz <hallo@iamschulz.de>
2022-11-03 13:39:02 +0100

    persist theme

--- src/ts/themeSwitch.ts
+++ src/ts/themeSwitch.ts
@@ -1,7 +1,6 @@
 export class ThemeSwitch {
 	private el: HTMLElement;
 	private toggle: HTMLInputElement;
-	private body: HTMLBodyElement;
 
 	constructor() {
 		const el = document.querySelector('[data-component="theme-switch"]');
@@ -13,20 +12,43 @@ export class ThemeSwitch {
 
 		this.el = el as HTMLElement;
 		this.toggle = toggle as HTMLInputElement;
-		this.body = document.body as HTMLBodyElement;
 		this.registerEvents();
 		this.enableUi();
 	}
 
 	private registerEvents() {
-		this.toggle.addEventListener("input", () => this.setBrightness());
+		this.toggle.addEventListener("input", () => {
+			this.setBrightness();
+			this.setColor();
+		});
 	}
 
 	private setBrightness() {
-		this.body.setAttribute("style", `--theme-lit: ${this.toggle.value}%`);
+		const brightness = this.toggle.value;
+		document.documentElement.style.setProperty(
+			"--theme-lit",
+			`${brightness}%`
+		);
+		window.localStorage.setItem("theme-brightness", brightness);
+	}
+
+	private setColor() {
+		const color = getComputedStyle(document.body)["background"];
+		(
+			document.head.querySelector(
+				'[name="theme-color"]'
+			) as HTMLMetaElement
+		).content = color;
+		window.localStorage.setItem("theme-color", color);
 	}
 
 	private enableUi() {
 		this.el.removeAttribute("hidden");
+		const lit = parseInt(
+			getComputedStyle(document.documentElement).getPropertyValue(
+				"--theme-lit"
+			)
+		).toString();
+		this.toggle.setAttribute("value", lit);
 	}
 }
