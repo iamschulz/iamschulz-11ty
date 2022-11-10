9b3e7a4e45eb0549e662ebfcc321043da8a35a05
Daniel Schulz <hallo@iamschulz.de>
2022-11-10 13:43:01 +0100

    add TOC highlighting

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,7 @@
 import { ReplaceIframe } from "./replaceIframe";
 import { ThemeSwitch } from "./themeSwitch";
+import { Toc } from "./toc";
 
 new ThemeSwitch();
 new ReplaceIframe();
+new Toc();
---
+++ src/ts/toc.ts
@@ -0,0 +1,88 @@
+export class Toc {
+	private el: HTMLElement;
+	private sections: {
+		navigation: HTMLAnchorElement;
+		elements: HTMLElement[];
+	}[];
+
+	constructor() {
+		const el = document.querySelector(
+			'[data-component="toc"]'
+		) as HTMLElement | null;
+
+		if (!el) {
+			return;
+		}
+
+		this.el = el;
+		this.sections = [];
+		this.getSections();
+		this.registerEvents();
+	}
+
+	getHeadlines(): HTMLElement[] {
+		return Array.from(this.el.querySelectorAll("a")).map((link) =>
+			document.querySelector(
+				`[id="${link.getAttribute("href")?.replace("#", "") || ""}"]`
+			)
+		) as HTMLElement[];
+	}
+
+	getSections() {
+		const elements = Array.from(
+			document.querySelectorAll(".article__content > *")
+		) as HTMLElement[];
+
+		elements.forEach((element) => {
+			if (element.matches(":is(h1, h2, h3") && element.id) {
+				const navigation = this.el.querySelector(
+					`[href="#${element.id}"]`
+				);
+
+				this.sections.push({
+					navigation: navigation as HTMLAnchorElement,
+					elements: [element],
+				});
+			} else {
+				this.sections.at(-1)?.elements.push(element);
+			}
+		});
+	}
+
+	registerEvents() {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					entry.target.setAttribute(
+						"data-in-screen",
+						String(entry.isIntersecting)
+					);
+				});
+				this.highlightNavigation();
+			},
+			{
+				threshold: 0,
+			}
+		);
+
+		const articleElements = this.sections.map((x) => x.elements).flat();
+		Object.keys(articleElements).forEach((index) => {
+			observer.observe(articleElements[index]);
+		});
+	}
+
+	highlightNavigation() {
+		window.requestAnimationFrame(() => {
+			this.sections.forEach((section) => {
+				section.navigation.setAttribute(
+					"data-highlight",
+					String(
+						section.elements.some((element) =>
+							element.matches('[data-in-screen="true"]')
+						)
+					)
+				);
+			});
+		});
+	}
+}
