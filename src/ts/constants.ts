430b5219c01726da8996490139c7f0a0252336f6
iamschulz <hallo@iamschulz.de>
2022-12-16 09:59:06 +0100

    fix constant urls

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,3 +1,3 @@
-export const apiProxy = "https://schulz2.uber.space/api-proxy?query=";
-export const getLikeApi = "https://iamschulz.com/like-api/get/";
-export const putLikeApi = "https://iamschulz.com/like-api/put/";
+export const apiProxy = 'https://schulz2.uber.space/api-proxy?query=';
+export const getLikeApi = 'https://schulz2.uber.space/like-api/get/';
+export const putLikeApi = 'https://schulz2.uber.space/like-api/put/';
