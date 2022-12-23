c15b5d6372dae7135d38afa7ca67c45d22080f85
Daniel Schulz <hallo@iamschulz.de>
2022-12-23 23:38:43 +0100

    add incremental cache to build step

--- src/_helpers/getCacheDuration.js
+++ src/_helpers/getCacheDuration.js
@@ -1,8 +1,8 @@
 module.exports = function () {
 	return {
-		db: process.env.CACHE_DB ? "2m" : "7d",
-		content: process.env.CACHE_CONTENT ? "2m" : "30d",
-		images: process.env.CACHE_IMAGES ? "10m" : "30d",
+		db: process.env.CACHE_DB ? "7d" : "2m",
+		content: process.env.CACHE_CONTENT ? "30d" : "2m",
+		images: process.env.CACHE_IMAGES ? "30d" : "10m",
 		articleId: null,
 	};
 };
