5e1859572935c17ac32236764900b2d847ffe1a8
Daniel Schulz <hallo@iamschulz.de>
2023-02-02 09:08:03 +0100

    add metrics

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,6 +5,7 @@ import { Toc } from "./toc";
 import { Preload } from "./preload";
 import { Reactions } from "./reactions";
 import { Share } from "./share";
+import { onLCP, onFID, onCLS } from "web-vitals";
 //import { TextAdventureLoader } from "./textAdventureLoader";
 
 new ReplaceIframe();
@@ -15,3 +16,8 @@ new Preload();
 new Reactions();
 new Share();
 //new TextAdventureLoader();
+
+console.log("testing metrics...");
+onLCP((lcp) => console.log("lcp", lcp));
+onFID((fid) => console.log("fid", fid));
+onCLS((cls) => console.log("cls", cls));
